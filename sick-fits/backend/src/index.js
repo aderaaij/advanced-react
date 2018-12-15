f9e3d2282633303f56d292b79ed4ce79e4d36af7
Arden de Raaij <deraaard@mbio-mbpro-95.home>
2018-12-15 14:58:48 +0000

    module 3 completed

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const servers = createServer();
+
+// TODO Use express middleware to handle cookies
+// TODO Use express middleware to populate current User
+servers.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`server is now running on port http://localhost:${deets.port}`);
+  }
+);
