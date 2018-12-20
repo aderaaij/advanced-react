a183054437fc09e767966210ebb312e5c80281f0
Arden de Raaij <deraaard@mbio-mbpro-95.home>
2018-12-20 20:44:13 +0000

    [add] lesson 25 & 26

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,24 @@
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const servers = createServer();
 
 servers.express.use(cookieParser());
-// TODO Use express middleware to populate current User
+
+// Decode the JWT so we can get the user ID on each request
+servers.express.use((req, res, next) => {
+  console.log('im a middleware');
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId onto the req for future req
+    req.userId = userId;
+  }
+  next();
+});
 servers.start(
   {
     cors: {
